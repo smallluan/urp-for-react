457e4a66c277909923d7f3afe86e62bf83987b48
ts <598051800@qq.com>
2025-10-30 22:03:38 +0800

    feat(Button): 初步搭建 Button 1.从该组件其，要把色彩系统，至少是明亮模式的色彩系统搞定

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ export default function App() {
 
   return (
     <div>
-      <UrpButton content='属性内'>标签内</UrpButton>
+      <h3>主题</h3>
+      <UrpButton theme="default">确定</UrpButton>
+      <hr/>
+      <UrpButton theme="primary">确定</UrpButton>
+      <hr/>
+      <UrpButton theme="warning">确定</UrpButton>
+      <hr/>
+      <UrpButton theme="success">确定</UrpButton>
+      <hr/>
+      <UrpButton theme="error">确定</UrpButton>
+      <hr/> 
+      <UrpButton size="large">确定</UrpButton>
+      <hr/>
+      <UrpButton size="small">确定</UrpButton>
+      <hr/>
+      <UrpButton shape="round">确定</UrpButton>
+      <hr/>
+      <UrpButton>确定</UrpButton>
+      <hr/>
     </div>
   )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,25 @@
 import { ButtonType } from "./type"
 import defaultProperties from "./propertiers.ts"
+import genClassNameFromProps from "../utils/tools/className.ts"
+import './style.less'
 
 export default function UrpButton(props: ButtonType) {
   const mergedProps = { ...defaultProperties, ...props }
+  const buttonClass = genClassNameFromProps(
+    {
+      variant: mergedProps.variant,
+      theme: mergedProps.theme,
+      shape: mergedProps.shape,
+      size: mergedProps.size
+    },
+    'urp-button',
+    'urp-button'
+  )
   // 优先显示已属性形式传进来的内容
   mergedProps.content = mergedProps.content ?? mergedProps.children
   return (
-    <div>{ mergedProps.content }</div>
+    <div className={buttonClass}>
+      <span>{ mergedProps.content }</span>
+    </div>
   )
 }
