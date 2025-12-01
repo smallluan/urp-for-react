0a8f8b7604a58d74f5ddb4e24a3d8f10c4ec018c
ts <598051800@qq.com>
2025-12-01 20:45:04 +0800

    feat(Space): 横纵间距约 gap 校验

--- src/components/Space/Space.tsx
+++ src/components/Space/Space.tsx
@@ -3,12 +3,21 @@ import { defaultProps } from "./properties.ts"
 import { SpaceProps } from "./type"
 import genClassNameFromProps from '../utils/tools/className.ts'
 import genStyleFromPrpos from '../utils/tools/style.ts'
+import { useMergedProps } from "../utils/tools/props.ts"
+import { formatProps } from './properties.ts'
+
+import { SpaceGap } from './type'
 
 import './style.less'
 
 const UrpSpace = (props: SpaceProps) => {
 
-  const mergedProps = { ...defaultProps, ...props }
+  const { merged: mergedProps } = useMergedProps(
+    defaultProps,
+    props,
+    ['direction', 'overflow', 'gap', 'align'],
+    formatProps
+  )
 
   const spaceClassName = useMemo(() => {
     return genClassNameFromProps(
@@ -22,8 +31,20 @@ const UrpSpace = (props: SpaceProps) => {
   }, [mergedProps.direction, mergedProps.overflow])
 
   const spaceStyle = useMemo(() => {
+    let horizontalGap: SpaceGap
+    let vertialGap: SpaceGap
+
+    if (Array.isArray(mergedProps.gap)) {
+      horizontalGap = mergedProps.gap[0]
+      vertialGap = mergedProps.gap[1]
+    } else {
+      horizontalGap = mergedProps.gap
+      vertialGap = mergedProps.gap
+    }
+    
     return genStyleFromPrpos({
-      'props-gap': mergedProps.gap,
+      'props-gap-vertial': vertialGap,
+      'props-gap-horizontal': horizontalGap,
       'props-align': mergedProps.align
     })
   }, [mergedProps.gap, mergedProps.align])
--- src/components/Space/properties.ts
+++ src/components/Space/properties.ts
@@ -7,3 +7,42 @@ export const defaultProps: SpaceProps = {
   overflow: 'breakLine',
   gap: '8px'
 }
+
+export const formatProps = (props: Required<SpaceProps>) => {
+
+  // 处理字符串类型的 gap
+  const handleStringGap = (gap: string) => {
+    gap = gap.trim()
+    if (!gap) return '0px'
+    const numberlize = Math.abs(Number(gap))
+    if (!isNaN(numberlize)) return numberlize + 'px'
+    if (CSS.supports('gap', gap)) {
+      return gap
+    }
+    return '8px'
+  }
+
+  // 处理数字和字符串类型的 gap
+  const handleStringOrNumberGap = (gap: string | number) => {
+    if (typeof gap === 'string') {
+      return handleStringGap(gap)
+    } else if (typeof gap === 'number') {
+      return Math.abs(gap) + 'px'
+    }
+    return '8px'
+  }
+
+  if (Array.isArray(props.gap)) {
+    if (!props.gap.length) {
+      props.gap = [0, 0]
+    } else if (props.gap.length === 1) {
+      props.gap[1] = props.gap[0]
+    }
+    props.gap = props.gap.slice(0, 2).map(item => {
+      return handleStringOrNumberGap(item)
+    })
+  } else {
+    props.gap = handleStringOrNumberGap(props.gap)
+  }
+  return props
+}
