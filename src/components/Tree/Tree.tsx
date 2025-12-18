16454744ee7684e1d38fa8660ebc0052cf5755d4
ts <598051800@qq.com>
2025-12-18 21:25:55 +0800

    feat(Tree): 组件折叠/展开实现与性能优化 1.强制添加唯一 key 用于优化，目前还没有添加对 key 的校验 2.通过 map 将寻找被点击元素索引复杂度降低至 O(1) 3.逆序入栈与函数式更新等优化，完善类型

--- src/components/Tree/Tree.tsx
+++ src/components/Tree/Tree.tsx
@@ -1,66 +1,209 @@
-import { useCallback, useMemo } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
 import "./style.less"
 import genStyleFromProps from "../utils/tools/style.ts"
 import { UIcon } from "../Icon/index.ts"
 
 // data 单节点
 type TreeOriginalNode = {
+  key: string;
   label: string;
   value?: string | number;
   children?: TreeOriginalNode[];
+
 }
 
 // 扁平化后的单个节点
 type TreeFlattenedNode = {
+  key: string;
   label: string;
   value?: string | number;
   level: string;
-  hasChildren: boolean
+  hasChildren: boolean;
+  parentNode: null | TreeFlattenedNode;
+  children: TreeOriginalNode[],
+  isOpen: boolean
 }
 
 const UTree = (props) => {
 
-  // 扁平化数组
-  const flatArray = useMemo(() => {
+  // 内部维护 扁平化数组，方便展开/收起节点
+  const [flatArray, setFlatArray] = useState<TreeFlattenedNode[]>(() => {
+    // 执行一次数组扁平化，处理第一层节点
     const originData: TreeOriginalNode[] = Array.isArray(props.data) ? props.data : []
     if (originData.length === 0) return []
-    const res: TreeFlattenedNode[] = []
-    const stack: [TreeOriginalNode, string][] = []
 
-    // 根节点逆序入栈，保证取出正序
-    for (let i = originData.length - 1; i >= 0; i--) {
+    const res: TreeFlattenedNode[] = []
+    const stack: [TreeOriginalNode, string, null | TreeFlattenedNode][] = []
+
+    // 根节点逆序入栈
+    for (let i = originData.length - 1; i >= 0; i --) {
       const node = originData[i]
       if (typeof node !== 'object' || node === null) continue
-      stack.push([node, '0'])
+      stack.push([node, '0', null])
     }
 
-    // 深度优先， stack 后进先出，使用效率更高的 pop 和 push 方法
-    while (stack.length > 0) {
-      const popped = stack.pop()
-      if (!popped) continue
-      const [node, level] = popped
-      // 字段兜底，避免 undefined
+    // 初始化仅处理根节点，子节点需要点击展开
+    while (stack.length) {
+      const poped = stack.pop()
+      if (!poped) continue
+
+      const [node, level, parentNode] = poped
       const label = node.label ?? ''
       const value = node.value ?? ''
       const children = Array.isArray(node.children) ? node.children : []
 
-      res.push({ label, value, level, hasChildren: !!children.length })
+      res.push({
+        ...node,
+        label,
+        value,
+        level,
+        hasChildren: !!children.length,
+        parentNode,
+        children,
+        isOpen: false
+      })
+    }
+    return res
+  })
 
-      // 子节点逆序入栈，保证取出时正序
-      for (let i = children.length - 1; i >= 0; i--) {
-        const child = children[i]
-        if (typeof child !== 'object' || child === null) continue
-        stack.push([child, String(Number(level) + 1)])
-      }
+  // key 索引映射表
+  const keyToIndexMap = useMemo(() => {
+    return flatArray.reduce((map, item, index) => {
+      map[item.key] = index
+      return map
+    }, {} as Record<string, number>)
+  }, [flatArray])
+
+  // props.data 变化是重新初始化节点
+  useEffect(() => {
+    const originData: TreeOriginalNode[] = Array.isArray(props.data) ? props.data : []
+    if (!originData.length) {
+      setFlatArray([])
+      return
     }
 
-    return res
-  }, [props.data])
+    const res: TreeFlattenedNode[] = []
+    const stack: [TreeOriginalNode, string, null | TreeFlattenedNode][] = []
+
+    for (let i = originData.length - 1; i >= 0; i--) {
+      const node = originData[i]
+      if (typeof node !== 'object' || node === null) continue
+      stack.push([node, '0', null])
+    }
+
+    while (stack.length) {
+      const popped = stack.pop()
+      if (!popped) continue
+      const [node, level, parentNode] = popped
+      const label = node.label ?? ''
+      const value = node.value ?? ''
+      const children = Array.isArray(node.children) ? node.children : []
+
+      res.push({
+        ...node,
+        label,
+        value,
+        level,
+        hasChildren: !!children.length,
+        parentNode,
+        children,
+        isOpen: false
+      })
+    }
+    setFlatArray(res)
+
+  }, [props.data]) 
 
   // item 的动态类
   const itemStyle = useCallback((level: string) => {
     return genStyleFromProps({ level })
   }, [])
+
+  const unfoldNode = useCallback((targetNode:TreeFlattenedNode, targetIndex: number) => {
+    const children = targetNode.children
+    if (!children.length) return
+
+    const stack: [TreeOriginalNode, string, TreeFlattenedNode | null][] = []
+    for (let i = children.length - 1; i >= 0; i --) {
+      const child = children[i]
+      if (typeof child !== 'object' || child === null) continue
+      stack.push([child, String(Number(targetNode.level) + 1), targetNode])
+    }
+
+    const newChildrenNodes: TreeFlattenedNode[] = []
+    while (stack.length) {
+      const popped = stack.pop()
+      if (!popped) continue
+      const [node, level, parent] = popped
+      const label = node.label ?? ''
+      const value = node.value ?? ''
+      const children = Array.isArray(node.children) ? node.children : []
+      newChildrenNodes.push({
+        ...node,
+        label,
+        value,
+        level,
+        hasChildren: !!children.length,
+        parentNode: parent,
+        children: children,
+        isOpen: false
+      })
+    }
+
+    // 将子节点插入到父节点的后边
+    setFlatArray(prev => {
+      const newArray = [...prev]
+      newArray.splice(targetIndex + 1, 0, ...newChildrenNodes)
+      newArray[targetIndex] = {
+        ...newArray[targetIndex],
+        isOpen: true
+      }
+      return newArray
+    })
+  }, [])
+
+  const foldNode = useCallback((targetNode: TreeFlattenedNode, targetIndex: number) => {
+  setFlatArray(prev => {
+    const targetLevel = Number(targetNode.level)
+    const deleteStartIndex = targetIndex + 1
+    let deleteEndIndex = deleteStartIndex
+
+    while (deleteEndIndex < prev.length) {
+      const currentNode = prev[deleteEndIndex]
+      const currentLevel = Number(currentNode.level)
+      
+      if (currentLevel <= targetLevel) break
+      deleteEndIndex++
+    }
+
+    const newArray = [...prev]
+    // 只有需要删除时才 splice
+    if (deleteEndIndex > deleteStartIndex) {
+      newArray.splice(deleteStartIndex, deleteEndIndex - deleteStartIndex)
+    }
+    // 重置展开状态
+    newArray[targetIndex] = {
+      ...newArray[targetIndex],
+      isOpen: false,
+    }
+    return newArray
+  })
+}, [])
+
+  // 点击 图标/node 时，展开或者收起
+  const handleClick = useCallback((clickedNode: TreeFlattenedNode) => {
+    // 找到被点击元素在 flatArray 中的索引
+    const targetIndex = keyToIndexMap[clickedNode.key]
+    if (targetIndex === -1) return  // 未找到目标元素
+    const targetNode = flatArray[targetIndex]
+    if (!targetNode.hasChildren) return  // 该元素没有孩子节点
+
+    if (!targetNode.isOpen) {
+      unfoldNode(targetNode, targetIndex)
+    } else {
+      foldNode(targetNode, targetIndex)
+    }
+  }, [flatArray, keyToIndexMap, foldNode, unfoldNode,])
 
   return (
     <div className="u-tree">
@@ -71,12 +214,13 @@ const UTree = (props) => {
             style={itemStyle(item.level)}
             data-value={item.value}
             data-level={item.level} 
-            key={item.label}
+            key={item.key}
           >
             <div className="u-tree-icon-container">
               {
                 item.hasChildren &&
                 <UIcon
+                  onClick={() => handleClick(item)}
                   className="u-tree-icon"
                   type="CaretRightOutlined"
                 />
