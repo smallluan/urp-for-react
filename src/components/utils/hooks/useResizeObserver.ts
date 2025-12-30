683c9ac44056e16e5224b87ac8a74a18497a0557
ts <598051800@qq.com>
2025-12-30 22:00:45 +0800

    feat(hooks): useResizeObserver

---
+++ src/components/utils/hooks/useResizeObserver.ts
@@ -0,0 +1,26 @@
+import { useState, useRef, useEffect } from "react"
+
+const useResizeObserver = () => {
+  const [contentRect, setContentRect] = useState<DOMRectReadOnly>()
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observerTarget = ref.current
+    if (!observerTarget) return
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      const entry = entries[0]
+      setContentRect(entry.contentRect)
+    })
+
+    resizeObserver.observe(observerTarget)
+
+    return () => {
+      resizeObserver.unobserve(observerTarget)
+    }
+  })
+
+  return { ref, contentRect }
+}
+
+export default useResizeObserver
